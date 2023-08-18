fb51c38392e506d31dfd3a3ef5da7fc62373e9d8
kosei <abdelhamid.boudjit@gmail.com>
2023-08-18 18:18:07 +0100

    change color to teal

--- client/src/ui/buttons/IconButton.tsx
+++ client/src/ui/buttons/IconButton.tsx
@@ -7,7 +7,7 @@ export function IconButton({ children, title, ...rest }: props) {
   return (
     <button
       title={title}
-      className="w-12 h-12 inline-flex justify-center items-center gap-2 rounded-md bg-pink-100 dark:hover:bg-[#ec489a68] dark:bg-[#ec489a3c] border border-transparent font-semibold text-pink-500 dark:text-pink-400  hover:bg-pink-200  focus:outline-none focus:ring-2 ring-offset-white focus:ring-pink-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-slate-900 aspect-square"
+      className="w-12 h-12 inline-flex justify-center items-center gap-2 rounded-md bg-teal-100 dark:hover:bg-teal-900 dark:bg-teal-900 border border-transparent font-semibold text-teal-500 dark:text-teal-400  hover:bg-teal-200  focus:outline-none focus:ring-2 ring-offset-white focus:ring-teal-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-slate-900 aspect-square"
       {...rest}
     >
       {children}
