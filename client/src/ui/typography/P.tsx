806a590bbd3805e9c3c78fc208ed7ec350fb0a3f
kosei <abdelhamid.boudjit@gmail.com>
2023-08-18 18:18:55 +0100

    add dynamic props

--- client/src/ui/typography/P.tsx
+++ client/src/ui/typography/P.tsx
@@ -1,5 +1,13 @@
-export function P({ children }: { children: React.ReactNode }) {
-  return <p className="text-xl dark:text-white">{children}</p>;
+type props = {
+  children: React.ReactNode;
+} & JSX.IntrinsicElements["h1"];
+
+export function P({ children, className, ...rest }: props) {
+  return (
+    <p className={`text-xl dark:text-white ${className}`} {...rest}>
+      {children}
+    </p>
+  );
 }
 
 export default P;
