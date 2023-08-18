6454d467af9b07a4c2cbcdd532110a31b45bb16f
kosei <abdelhamid.boudjit@gmail.com>
2023-08-18 18:18:37 +0100

    add dynamic props

--- client/src/ui/typography/H1.tsx
+++ client/src/ui/typography/H1.tsx
@@ -1,6 +1,13 @@
-export function H1({ children }: { children: React.ReactNode }) {
+type props = {
+  children: React.ReactNode;
+} & JSX.IntrinsicElements["h1"];
+
+export function H1({ children, className, ...rest }: props) {
   return (
-    <h1 className="text-4xl md:text-6xl font-bold text-center dark:text-white">
+    <h1
+      className={`text-2xl md:text-4xl font-bold text-center ${className}`}
+      {...rest}
+    >
       {children}
     </h1>
   );
