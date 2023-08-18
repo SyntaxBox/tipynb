8eddbfdb63f7a01354eda0d761567ef968a35002
kosei <abdelhamid.boudjit@gmail.com>
2023-08-18 18:18:22 +0100

    reduce size

--- client/src/ui/layout/Container.tsx
+++ client/src/ui/layout/Container.tsx
@@ -8,7 +8,7 @@ export function Container({ children, className, ...rest }: props) {
     <div className="px-4 sm:px-4 py-2.5 rounded z-1 h-full w-full">
       <div
         {...rest}
-        className={`h-full w-full container mx-auto max-w-6xl ${className}`}
+        className={`h-full w-full container mx-auto max-w-4xl ${className}`}
       >
         {children}
       </div>
