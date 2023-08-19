7b0d8e10245ea62f4a07c84ec480cddd344fb207
kosei <abdelhamid.boudjit@gmail.com>
2023-08-19 17:44:35 +0100

    add disabled colors

--- client/src/ui/buttons/textButton.tsx
+++ client/src/ui/buttons/textButton.tsx
@@ -8,7 +8,7 @@ type props = {
 export function TextButton({ children, className, ...rest }: props) {
   return (
     <button
-      className={`py-4 px-8 inline-flex justify-center items-center gap-2 rounded-md bg-pink-500 border border-transparent font-semibold text-white text-xl  hover:bg-pink-600 focus:outline-none focus:ring-2 ring-offset-white focus:ring-pink-500 focus:ring-offset-2 transition-all dark:focus:ring-offset-gray-800 ${className}`}
+      className={`py-4 px-8 inline-flex justify-center items-center gap-2 rounded-md bg-pink-500 border border-transparent font-semibold text-white text-xl  hover:bg-pink-600 focus:outline-none focus:ring-2 ring-offset-white focus:ring-pink-500 focus:ring-offset-2 transition-all dark:focus:ring-offset-gray-800 disabled:bg-pink-300 disabled:dark:bg-pink-800 ${className}`}
       {...rest}
     >
       {children}
