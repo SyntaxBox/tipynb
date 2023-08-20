a7e5971b32744f471b6eb27533fa1278911eddd1
kosei <abdelhamid.boudjit@gmail.com>
2023-08-20 21:08:45 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,20 @@
-import * as express from 'express';
 import { Express, Request, Response } from 'express';
+import * as express from 'express';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+import apiRouter from './routes/api';
+import { corsConfig } from './config';
 
 dotenv.config({ path: __dirname + '/.env' });
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors(corsConfig));
+app.use('/api', apiRouter);
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('⚡️ Express + TypeScript Server');
+  res.json({ data: '⚡️ Express + TypeScript Server' });
 });
 
 app.listen(port, () => {
