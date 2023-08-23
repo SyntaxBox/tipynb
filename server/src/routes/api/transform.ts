87a1185c1237ccd1755b090a9bc9b52582bee8a5
kosei <abdelhamid.boudjit@gmail.com>
2023-08-23 16:10:40 +0100

    Update transform.ts

--- server/src/routes/api/transform.ts
+++ server/src/routes/api/transform.ts
@@ -5,8 +5,8 @@ export default async function transform(req: Request, res: Response) {
   const { file, body } = req;
   const { pdf, py, docx } = body;
 
-  const fileName = file.originalname;
-  const fileExtension = fileName.split('.')[fileName.split('.').length - 1];
+  const filename = file.originalname;
+  const fileExtension = filename.split('.')[filename.split('.').length - 1];
 
   if (fileExtension !== 'ipynb')
     res.status(400).json({ error: 'Bad Request: file is not a .ipynb file' });
@@ -14,6 +14,17 @@ export default async function transform(req: Request, res: Response) {
   if (!pdf && !py && !docx)
     res.status(400).json({ error: 'Bad Request: No return File selected' });
 
-  const data = transformIPYNB(file, { pdf, py, docx });
+  const data = transformIPYNB(
+    filename
+      .split('.')
+      .splice(filename.split('.').length - 1, 1)
+      .join('.'),
+    file,
+    {
+      pdf,
+      py,
+      docx,
+    },
+  );
   res.json({ data });
 }
