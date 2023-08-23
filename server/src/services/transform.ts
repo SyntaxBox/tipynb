18021f80d9d7fa48bc1383051a9696638765472b
kosei <abdelhamid.boudjit@gmail.com>
2023-08-23 16:10:43 +0100

    Update transform.ts

--- server/src/services/transform.ts
+++ server/src/services/transform.ts
@@ -1,4 +1,7 @@
+import { Transform } from '../utils';
+
 export function transformIPYNB(
+  fileName: string,
   buffer: Express.Multer.File,
   options: {
     pdf?: boolean;
@@ -6,20 +9,28 @@ export function transformIPYNB(
     docx?: boolean;
   },
 ) {
+  const { pdf, py, docx } = options;
+  if (!pdf && !py && !docx) throw new Error('no file type selected');
+
   const file = buffer.buffer.toString();
   const notebook = JSON.parse(file);
   let pythonCode = '';
-  let markdownContent = '';
+  let markdown = '';
 
   // Extract Python code and Markdown cells
   notebook.cells.forEach((cell) => {
     if (cell.cell_type === 'code') {
       pythonCode += cell.source + '\n\n';
     } else if (cell.cell_type === 'markdown') {
-      markdownContent += cell.source + '\n\n';
+      markdown += cell.source + '\n\n';
     }
   });
   console.log({ pythonCode });
-  console.log({ markdownContent });
-  return buffer;
+  console.log({ markdown });
+  const zipFile = Transform.generateZipped(
+    fileName,
+    { pythonCode, markdown },
+    { pdf: true, py, docx },
+  );
+  return zipFile;
 }
